bfaad71482844a1559c09ab6661acbf4f73d76bd
Moritz Timmer <25349452+Moehre2@users.noreply.github.com>
2024-06-04 19:06:47 +0200

    add MulToAdd shell

--- tools/tutorial-opt.cpp
+++ tools/tutorial-opt.cpp
@@ -1,4 +1,5 @@
 #include "lib/Transform/Affine/AffineFullUnroll.hpp"
+#include "lib/Transform/Arith/MulToAdd.hpp"
 #include "mlir/InitAllDialects.h"
 #include "mlir/Pass/PassManager.h"
 #include "mlir/Pass/PassRegistry.h"
@@ -9,5 +10,6 @@ int main(int argc, char **argv) {
     mlir::registerAllDialects(registry);
     mlir::PassRegistration<mlir::tutorial::AffineFullUnrollPass>();
     mlir::PassRegistration<mlir::tutorial::AffineFullUnrollPassAsPatternRewrite>();
+    mlir::PassRegistration<mlir::tutorial::MulToAddPass>();
     return mlir::asMainReturnCode(mlir::MlirOptMain(argc, argv, "Tutorial Pass Driver", registry));
 }
