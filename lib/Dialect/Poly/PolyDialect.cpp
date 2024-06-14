5a94306712d64f2ca4dbe267a122a08daf8847b0
Moritz Timmer <25349452+Moehre2@users.noreply.github.com>
2024-06-14 15:45:25 +0200

    Add empty Poly dialect

---
+++ lib/Dialect/Poly/PolyDialect.cpp
@@ -0,0 +1,17 @@
+#include "lib/Dialect/Poly/PolyDialect.hpp"
+
+#include "lib/Dialect/Poly/PolyDialect.cpp.inc"
+
+namespace mlir {
+
+    namespace tutorial {
+
+        namespace poly {
+
+            void PolyDialect::initialize() {}
+
+        }
+
+    }
+
+}
---
+++ lib/Dialect/Poly/PolyDialect.hpp
@@ -0,0 +1,8 @@
+#ifndef LIB_DIALECT_POLY_POLYDIALECT_H_
+#define LIB_DIALECT_POLY_POLYDIALECT_H_
+
+#include "mlir/IR/DialectImplementation.h"
+
+#include "lib/Dialect/Poly/PolyDialect.h.inc"
+
+#endif
