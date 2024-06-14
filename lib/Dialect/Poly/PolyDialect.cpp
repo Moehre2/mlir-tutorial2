0ad177dec5a5282fd8b2ec4e6c48783d69dc65f6
Moritz Timmer <25349452+Moehre2@users.noreply.github.com>
2024-06-14 16:11:16 +0200

    Add a poly type

--- lib/Dialect/Poly/PolyDialect.cpp
+++ lib/Dialect/Poly/PolyDialect.cpp
@@ -1,6 +1,12 @@
 #include "lib/Dialect/Poly/PolyDialect.hpp"
 
+#include "lib/Dialect/Poly/PolyTypes.hpp"
+#include "mlir/IR/Builders.h"
+#include "llvm/ADT/TypeSwitch.h"
+
 #include "lib/Dialect/Poly/PolyDialect.cpp.inc"
+#define GET_TYPEDEF_CLASSES
+#include "lib/Dialect/Poly/PolyTypes.cpp.inc"
 
 namespace mlir {
 
@@ -8,7 +14,12 @@ namespace mlir {
 
         namespace poly {
 
-            void PolyDialect::initialize() {}
+            void PolyDialect::initialize() {
+                addTypes<
+                    #define GET_TYPEDEF_LIST
+                    #include "lib/Dialect/Poly/PolyTypes.cpp.inc"
+                >();
+            }
 
         }
 
---
+++ lib/Dialect/Poly/PolyTypes.hpp
@@ -0,0 +1,9 @@
+#ifndef LIB_TYPES_POLY_POLYTYPES_H_
+#define LIB_TYPES_POLY_POLYTYPES_H_
+
+#include "mlir/IR/DialectImplementation.h"
+
+#define GET_TYPEDEF_CLASSES
+#include "lib/Dialect/Poly/PolyTypes.h.inc"
+
+#endif
